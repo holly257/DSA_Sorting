067be714a18be89e03220d6c139b792e2ebd6436
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-08-01 13:52:12 -0400

    working on #5 with recursion call

--- LinkedList.js
+++ LinkedList.js
@@ -180,11 +180,22 @@ class linkedList {
         }
         previousNode.next = currNode.next;
     }
+
+    removeHead() {
+        // If the list is empty
+        if (!this.head) {
+            return null;
+        }
+       
+        let curr = this.head
+        this.head = this.head.next;
+        return curr
+    }
 }
 
 //5. sort linked list using merge
-let data = [30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5]
-
+//let data = [30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5]
+let data = [5,1,3,6,2,8,7,4]
 function main(){
     const linkedL = new linkedList()
     data.forEach(item => linkedL.insertLast(item))
@@ -193,26 +204,38 @@ function main(){
     return linkedL
 }
 //merge sort
-function mergeData(left, right, array){
-    let leftIndex = 0;
-    let rightIndex = 0;
-    let outputIndex = 0;
-
-    while(leftIndex < left.length && rightIndex < right.length){
-        if(left[leftIndex] < right[rightIndex]) {
-            array[outputIndex++] = left[leftIndex++];
+function mergeData(left, right, list){
+    while(!left.isEmpty() && !right.isEmpty()){
+        if(left.head.value < right.head.value){
+            let curr = removeHead(left)
+            list.insertLast(curr)
         } else {
-            array[outputIndex++] = right[rightIndex++]
+            let curr = removeHead(right)
+            list.insertLast(curr)
         }
     }
-    for(let i = leftIndex; i < left.length; i++) {
-        array[outputIndex++] = left[i]
+    
+    while(!left.isEmpty()){
+        let curr = removeHead(left)
+        list.insertLast(curr)
     }
 
-    for(let i = rightIndex; i < right.length; i++) {
-        array[outputIndex++] = right[i];
+    while(!right.isEmpty()){
+        let curr = removeHead(right)
+        list.insertLast(curr)
     }
-    return array;
+    
+    return list;
+}
+function removeHead(list) {
+    if (!list.head) {
+        return null;
+    }
+   
+    let curr = list.head
+    list.head = list.head.next;
+    curr.next = null;
+    return curr
 }
 
 function getLinkedListLength(list){
@@ -226,24 +249,38 @@ function getLinkedListLength(list){
     return count;
 }
 
+function isEmpty(list) {
+    if (!list.head) {
+        return true;
+    } else return false;
+}
+
 function mergeSort(list) {
     const length = getLinkedListLength(list)
-    console.log(length)
+    const mid = Math.floor(length/2)
+
+    let final = new linkedList();
+    let right = list.head
+    let curr
+    let count =0;
+
+    while(right.next && count < mid){
+        count = count + 1
+        curr = right
+        right = right.next
+    }
+
+    curr.next = null;    
 
     if (length <= 1) {
         return list;
     }
 
-    const middle = Math.floor(length / 2);
-
     //https://www.youtube.com/watch?v=KB31cbIqRPs
-    //talked to Vincent, different approach than ^
-    // let left = array.slice(0, middle);
-    // let right = array.slice(middle, array.length);
 
-    // left = mergeSort(left);
-    // right = mergeSort(right);
-    //return mergeData(left, right, array);
+    list = mergeSort(list);
+    right = mergeSort(right);
+    return mergeData(list, right, final);
 }
 
 //console.log(newData)
